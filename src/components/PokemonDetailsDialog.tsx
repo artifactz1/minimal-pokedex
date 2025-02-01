06e45e21cdacb5e50c64f1882fafb6d04f551268
artifactz1 <punsalang.art@gmail.com>
2025-02-01 06:35:56 -0800

    added id inside pokemon details card

--- src/components/PokemonDetailsDialog.tsx
+++ src/components/PokemonDetailsDialog.tsx
@@ -1,4 +1,4 @@
-import { Grid, Maximize, Minimize, X } from "lucide-react";
+import { Grid, IdCard, Maximize, Minimize, X } from "lucide-react";
 import {
   Dialog,
   DialogContent,
@@ -49,7 +49,7 @@ export default function PokemonDetailsDialog({
             </div>
           </div>
           <div className="mt-6 grid grid-cols-2 gap-4">
-            <div>
+            <div className="space-y-2">
               <h3 className="mb-2 text-xl font-bold uppercase">Details</h3>
               <p className="mb-2 flex items-center">
                 <Grid className="mr-2 h-5 w-5 flex-shrink-0" />
@@ -60,6 +60,11 @@ export default function PokemonDetailsDialog({
                 <Maximize className="mr-2 h-5 w-5 flex-shrink-0" />
                 <span className="mr-2 font-bold uppercase">Weight:</span>
                 {pokemon.weight / 10}kg
+              </p>
+              <p className="flex items-center">
+                <IdCard className="mr-2 h-5 w-5 flex-shrink-0" />
+                <span className="mr-2 font-bold uppercase">ID:</span>
+                {pokemon.id}
               </p>
             </div>
             <div>
